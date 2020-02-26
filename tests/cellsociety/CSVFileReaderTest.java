bdcf43ef3a324ec1c2dbf9aa30328268d78dca4b
nebularthm <mw376@duke.edu>
2020-02-26 16:48:34 -0500

    starting to add tests for the controller-model side methods

---
+++ tests/cellsociety/CSVFileReaderTest.java
@@ -0,0 +1,24 @@
+package cellsociety;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CSVFileReaderTest {
+
+    @Test
+    void readGame() {
+    }
+
+    @Test
+    void getHeight() {
+    }
+
+    @Test
+    void getWidth() {
+    }
+
+    @Test
+    void readStates() {
+    }
+}
